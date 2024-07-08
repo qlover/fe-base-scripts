9f78fd874ee855bf176f06b9534d6fc6d6f7cb06
qlover <myused@sina.com>
2024-07-08 15:32:38 +0800

    chore: release-pr controller

--- packages/bin/release.js
+++ packages/bin/release.js
@@ -4,21 +4,12 @@ import { Release } from '../lib/index.js';
 import { ReleaseConfig } from '../lib/releaseConfig.js';
 
 async function main() {
-  this.log.log('Publishing to NPM and GitHub...');
-
   const releaseConfig = new ReleaseConfig().setup();
+  releaseConfig.log.log('Publishing to NPM and GitHub...');
 
   const release = new Release(releaseConfig);
 
   await release.releaseIt();
-
-  // const { tagName, releaseBranch } = await release.createReleaseBranch();
-
-  // const prNumber = await release.createReleasePR(tagName, releaseBranch);
-
-  // await release.autoMergePR(prNumber);
-
-  // await release.checkedPR(prNumber, releaseBranch);
 
   releaseConfig.log.success('Release successfully');
 }
