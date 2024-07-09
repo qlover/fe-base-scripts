e7ab88acec5c60ae7bc1fb8b399615cdbbb8748d
QRenjie <131101783+QRenjie@users.noreply.github.com>
2024-07-09 13:23:26 +0800

    ci: fix new flow (#60)

--- packages/lib/relesae.js
+++ packages/lib/relesae.js
@@ -36,8 +36,8 @@ export class Release {
     // 2. no publish github/release/tag
     if (this.config.isCreateRelease) {
       command.push(
-        '--no-git.tag --no-git.push --no-npm.publish'
-        // '--no-npm.publish --no-git.push --no-github.publish --no-github.release'
+        '--no-git.tag --no-git.push --no-npm.publish --no-github.release'
+        // '--no-npm.publish --no-git.tag --no-git.push --no-github.publish --no-github.release'
       );
     }
     // use current pkg, no publish npm and publish github
