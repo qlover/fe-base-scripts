2f25a089e929038c042e393e697da34414f4282f
qlover <myused@sina.com>
2024-07-10 10:44:32 +0800

    feat: fe-scripts config type

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -8,17 +8,115 @@ declare module '@qlover/fe-scripts' {
     config: SearchConfigType;
   };
 
+  export type ReleaseConfig = {
+    /**
+     * Whether to automatically merge PR when creating and publishing
+     *
+     * @default true
+     */
+    autoMergeReleasePR?: boolean;
+
+    /**
+     * PR auto merged type
+     *
+     * @default merge
+     */
+    autoMergeType?: 'merge' | 'squash' | 'rebase';
+    /**
+     * Create the branch name of PR when publishing
+     *
+     * @default ${env}-${branch}-${tagName}
+     */
+    branchName?: string;
+    /**
+     * Create a title for publishing PR
+     *
+     * @default [Bot Release] Branch:${branch}, Tag:${tagName}, Env:${env}
+     */
+    PRTitle?: string;
+    /**
+     * Create a body for publishing PR
+     *
+     * @default This PR includes version bump to ${tagName}
+     */
+    PRBody?: string;
+
+    /**
+     * Create a label for publishing PR
+     */
+    label?: {
+      /**
+       * hex color string
+       *
+       * @default 1A7F37
+       */
+      color?: string;
+
+      /**
+       * @default Label for version update PRs
+       */
+      description?: string;
+
+      /**
+       * @default CI-Release
+       */
+      name?: string;
+    };
+  };
+
   export type FeScriptsConfig = {
-    protectedBranches?: string;
+    /**
+     * Run `fe-clean-branch` to exclude branches
+     *
+     * @default ["master", "develop", "main"]
+     */
+    protectedBranches?: string[];
+    /**
+     * Run `fe-clean` to includes files
+     *
+     * @default ["dist","node_modules","yarn.lock","package-lock.json",".eslintcache","*.log"]
+     */
     cleanFiles?: string[];
-    author?: string;
-    repository?:
+    /**
+     * Use author name when create merge PR
+     *
+     * @default `package.json -> autor`
+     */
+    author?:
       | string
       | {
           name?: string;
           email?: string;
         };
+    /**
+     * Use repo info when create merge PR
+     *
+     * @default `package.json -> repository`
+     */
+    repository?:
+      | string
+      | {
+          url?: string;
+          [key: string]: string;
+        };
+    /**
+     * commitlint config
+     *
+     * @default { "extends": ["@commitlint/config-conventional"] }
+     */
     commitlint?: import('@commitlint/types').UserConfig;
+
+    /**
+     * release-it config
+     *
+     * **this config propery is currently not supported! Using `release-it` native usage**
+     * @default {}
+     */
     releaseIt?: import('release-it').Config;
+
+    /**
+     * config of CI release
+     */
+    release?: ReleaseConfig;
   };
 }
